d385e90fffb937cba71051abf75681e98e05159f
zqzal <936952020@qq.com>
2019-08-08 08:59:35 +0800

    Litepal init

--- app/src/main/java/com/xuxi/litepal/MainActivity.java
+++ app/src/main/java/com/xuxi/litepal/MainActivity.java
@@ -3,6 +3,10 @@ package com.xuxi.litepal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createDatabase = findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LitePal.getDatabase();
+            }
+        });
+
     }
 }
